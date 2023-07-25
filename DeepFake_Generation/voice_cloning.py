be11dd2896182a798d53d89bf63e2fe4a083ba75
Hashim Ali <hashim_ali94@outlook.com>
2023-07-24 20:49:04 -0400

    usinf pandas dataframe to write script csv files

--- DeepFake_Generation/voice_cloning.py
+++ DeepFake_Generation/voice_cloning.py
@@ -1,6 +1,7 @@
 import os
+import pandas as pd
 
-from elevenlabs import voices, generate, set_api_key, play
+from elevenlabs import voices, generate, set_api_key, play, Models
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -12,8 +13,29 @@ set_api_key(elevenlabs_api_key)
 voices = voices()
 
 # print(voices)
+
+models = Models.from_api()
+
+# print(models)
+
+# for i, voice in enumerate(voices):
+
+#     print(i)
+#     print(voice)
+
+# print(voices)
 # print(type(voices))
 
-audio = generate(text="Hi! My name is Bella, nice to meet you!", voice=voices[9])
+# spkr_map = {'Obama': 'Barack_Obama', 'Elon Musk': 'Elon_Musk', 'Trump': 'Donald_Trump'}
 
-play(audio)
+speaker_name = 'Barack_Obama' # Obama, Trump, Elon Musk, 
+text_file_path = '../Famous_Figures/Test_no_sentiments/Obamametadata.csv'
+dst_path = '../Famous_Figures/Test_no_sentiments/' + speaker_name + 'ElevenLabs/' 
+
+text_pd = pd.read_csv(text_file_path, sep='|')
+
+print(text_pd)
+
+# audio = generate(text="Hi! My name is Bella, nice to meet you!", voice=speaker_name)
+
+# play(audio)
